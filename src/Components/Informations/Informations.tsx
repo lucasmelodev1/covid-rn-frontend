4e8cb9b8cf0a1d05a60a2fe99403c92288933c83
Lucas Gabriel de Melo Rodrigues <lucasgabrielmelorodrigues@gmail.com>
2021-04-28 22:00:30 -0300

    production fixing

--- src/Components/Informations/Informations.tsx
+++ src/Components/Informations/Informations.tsx
@@ -22,7 +22,7 @@ export default function Information() {
     }
 
     var data = ''
-    const response = axios.get(`https://covid-rn-backend.herokuapp.com/api/cities/${details.city}`)
+    const response = axios.get(`${process.env.API_HOST}${details.city}`)
     response.then((data) => {
       setLoading(false)
       setMelhoria(data.data.improvementRate)
